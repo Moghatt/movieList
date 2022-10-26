9e8c5be45fc89410871be415eb7cb26b492ac51f
moghatt <mohammad_ghatali_kh@yahoo.com>
2022-10-27 09:39:03 +1030

    movielist

---
+++ frontend/src/pages/SearchMovies.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from "axios";
+
+import MovieFilters from "../components/MovieFilters";
+import MovieList from "../components/MovieList";
+const omdbApiKey = 'c7e7efd1'
+export default function SearchMovies(props) {
+  console.log(process.env.API_KEY)
+    const { handleAddToList, savedMoviesMap, handleRemoveFromList } = props;
+    const [movies, setMovies] = useState([]);
+
+    const handleSearchMovies = (queries) => {
+        const { s } = queries;
+        
+        axios.get(`http://www.omdbapi.com/?s=${s}&apikey=${omdbApiKey}`)
+            .then((res) => {
+                if (Array.isArray(res.data.Search)) {
+                    setMovies(res.data.Search.map((s) => {
+                        return {
+                            imdbID: s.imdbID,
+                            title: s.Title,
+                            year: s.Year,
+                            poster: s.Poster,
+                            type: s.Type
+                        }
+                    }))
+                }
+            }).catch((err) => {
+                alert(err);
+            })
+    }
+
+
+    return (
+        <div>
+            <MovieFilters handleSearchMovies={handleSearchMovies} />
+            <MovieList handleAddToList={handleAddToList} data={movies} savedMoviesMap={savedMoviesMap} handleRemoveFromList={handleRemoveFromList} />
+        </div>
+    )
+}
